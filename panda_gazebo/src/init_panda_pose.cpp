e2e8928c4c12faf359527aa9de12d157681cdb10
Vincent Mayer <vincent.mayer@agile-robots.com>
2020-12-04 14:28:28 +0100

    fix: Joint state wrapper is now equipped with hithand and panda joints by default

--- panda_gazebo/src/init_panda_pose.cpp
+++ panda_gazebo/src/init_panda_pose.cpp
@@ -5,6 +5,7 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "init_panda_pose");
     ros::NodeHandle n;
     // Instantiate subscribers for each joint not supposed to be zero
+    ros::Publisher j1_pub = n.advertise<std_msgs::Float64>("panda_hithand/panda_j1_position_controller/command", 1000);
     ros::Publisher j2_pub = n.advertise<std_msgs::Float64>("panda_hithand/panda_j2_position_controller/command", 1000);
     ros::Publisher j4_pub = n.advertise<std_msgs::Float64>("panda_hithand/panda_j4_position_controller/command", 1000);
     ros::Publisher j6_pub = n.advertise<std_msgs::Float64>("panda_hithand/panda_j6_position_controller/command", 1000);
@@ -14,6 +15,8 @@ int main(int argc, char **argv){
     ros::Rate loop_rate(1);
     int count = 0;
     // Initial pose
+    std_msgs::Float64 j1_msg;
+    j1_msg.data = -1.5708;
     std_msgs::Float64 j2_msg;
     j2_msg.data = -0.553876;
     std_msgs::Float64 j4_msg;
@@ -24,7 +27,8 @@ int main(int argc, char **argv){
     j7_msg.data = -0.785;
 
     while(ros::ok() && count < 5){
-        j2_pub.publish(j2_msg);
+	j1_pub.publish(j1_msg);        
+	j2_pub.publish(j2_msg);
         j4_pub.publish(j4_msg);
         j6_pub.publish(j6_msg);
         j7_pub.publish(j7_msg);
--- panda_gazebo/src/robot_joint_wrapper.cpp
+++ panda_gazebo/src/robot_joint_wrapper.cpp
@@ -14,8 +14,13 @@ using namespace std;
 bool got_joints_=false, publishers_init_=false, got_control_method_=false, notified_user_=false;
 int n_joints_;
 char control_method_;
-vector<string> joint_names_;
 map<string, ros::Publisher> joint_pub_;
+vector<string> joint_names_;
+
+vector<string> panda_joint_names {"panda_j1", "panda_j2", "panda_j3", "panda_j4", "panda_j5", "panda_j6", "panda_j7"};
+vector<string> hithand_joint_names {"thumb1", "thumb2", "thumb3", "thumb4", "index1", "index2", "index3", "index4",  "middle1",  "middle2",  "middle3",  "middle4",  "ring1",  "ring2",  "ring3",  "ring4",  "little1",  "little2",  "little3",  "little4",};
+
+// Listen to: /hithand/joint_cmd , then split this command and publish to /hithand/middle1_position_controller/command
 
 /**
  * Check if a commanded control mode is valid
@@ -49,16 +54,16 @@ void jointCallback(sensor_msgs::JointState jc)
 {
   if(got_control_method_)
   {
-    n_joints_ = jc.name.size();
-    if(!got_joints_)
-    {
-      ROS_INFO("[RobotJointWrapper] Got joints for the first time!");
-    }
-    got_joints_ = true;
-    joint_names_ = jc.name;
+    bool command_size_correct;
     vector<double> position=jc.position;
     vector<double> velocity=jc.velocity;
     vector<double> effort=jc.effort;
+    if((position.size() == n_joints_) || (velocity.size() == n_joints_) || (effort.size() == n_joints_)){
+      command_size_correct = true;
+    }
+    if(!command_size_correct){
+      ROS_ERROR_STREAM("The command had the wrong size. Size of command should be equal to number of controlled joints:" << n_joints_);
+    }
     if(publishers_init_)
     {
       for(int i=0;i<n_joints_;i++)
@@ -90,8 +95,8 @@ void jointCallback(sensor_msgs::JointState jc)
       // This is so you you don't continually yell at the user, which happens in Orocos sim
       if (!notified_user_)
       {
-	ROS_INFO("[RobotJointWrapper] Need to initialize publishers!");
-	notified_user_ = true;
+	      ROS_INFO("[RobotJointWrapper] Need to initialize publishers!");
+	      notified_user_ = true;
       }
     }
   }
@@ -131,18 +136,35 @@ int main(int argc,char* argv[])
   std::string jnt_cmd_topic;
 
   n_private.param<std::string>("listen_prefix", listen_prefix,"/panda");
-  n_private.param<std::string>("publish_prefix", publish_prefix,"/panda");
-  n_private.param<std::string>("jnt_cmd_topic", jnt_cmd_topic, listen_prefix + "/joint_cmd");
+  n_private.param<std::string>("publish_prefix", publish_prefix,"/panda"); // PUBLISH: /hithand/JOINT_position_controller/command
+  n_private.param<std::string>("jnt_cmd_topic", jnt_cmd_topic, listen_prefix + "/joint_cmd"); // LISTEN: /hithand/joint_cmd
 
-  cout << "Listen prefix:" << listen_prefix;
-  cout << "Publish prefix:" << publish_prefix;
+  listen_prefix.insert(0, 1, '/');
+  publish_prefix.insert(0, 1, '/');
+
+  //cout << "Listen prefix:" << listen_prefix;
+  //cout << "\n Publish prefix:" << publish_prefix;
+
+  // this section just assigns the hard coded panda/hithand joint names
+  if(publish_prefix == "/panda_hithand"){
+    joint_names_ = panda_joint_names;
+    n_joints_ = joint_names_.size();
+  }
+  else if(publish_prefix == "/hithand"){
+    joint_names_ = hithand_joint_names;
+    n_joints_ = joint_names_.size();
+  }
+  else{
+    ROS_ERROR_STREAM("Unknown publish prefix, only /panda or /hithand currently available. If using different robot or hand, change this script");
+  }
+  got_joints_ = true;
 
   // Setup subscribers
   stringstream topic_name;
   topic_name << listen_prefix << "/control_type";
-  ros::Subscriber control_sub=n.subscribe(topic_name.str(), 100, controlCallback);
+  ros::Subscriber control_sub=n.subscribe(topic_name.str(), 100, controlCallback); // Change the control type with this
 
-  ros::Subscriber joint_sub=n.subscribe(jnt_cmd_topic, 100, jointCallback);
+  ros::Subscriber joint_sub=n.subscribe(jnt_cmd_topic, 100, jointCallback); // This listens to the data on /robot/joint_cmd
 
   // Change the loop rate as required
   int loop_rate_int = 100;
@@ -192,6 +214,7 @@ int main(int argc,char* argv[])
         joint_pub_[joint_names_[i]]=n.advertise<std_msgs::Float64>(pub_name.str(), 1);
       }
       publishers_init_ = true;
+      ROS_INFO_STREAM("Publishers are now INITIALIZED, RECEIVED JOINT NAMES!");
     }
     ros::spinOnce();
     loop_rate.sleep();
